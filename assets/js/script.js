484a7b2c8843760a542a88195efdb2125bbe93e4
Scott Nichols <s21nichols@hotmail.com>
2021-11-20 23:53:15 -0700

    attached momentJS and displayed current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ function generateTimeblock (hour, value) {
   var timeBlockSave = $("<div>")
   timeBlockSave.addClass("col-2 ml-2 bg-info text-light text-center p-2")
   var saveIcon = $("<i>")
-  saveIcon.addClass("fas fa-save fa-5x")
+  saveIcon.addClass("fas fa-save fa-4x")
   timeBlockSave.append(saveIcon)
 
 
@@ -27,6 +27,15 @@ function generateTimeblock (hour, value) {
   return timeBlockRow
 }
 
-for (let i = 0; i < 9; i++) {
-  $("#timeBlocks").append(generateTimeblock(i+10, "")
+var timeIncrementArray = ["8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 AM","1:00 PM",
+ "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM"] 
+
+for (let i = 0; i < timeIncrementArray.length; i++) {
+  $("#timeBlocks").append(generateTimeblock([i], "")
   )}
+
+//creating current date + Time
+var timeDisplay = moment();
+var timeContainer = $("#currentDay");
+timeContainer.css("fontWeight", "bold")
+timeContainer.append(timeDisplay.format("dddd, MMMM Do YYYY, h:mm:ss a"));
