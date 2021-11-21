6cd72f67a6543c2a39d060c154b37ad8a0ba3a86
Scott Nichols <s21nichols@hotmail.com>
2021-11-21 01:46:01 -0700

    Added hourly display

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,9 +29,8 @@ function generateTimeblock (hour, value) {
 
 var timeIncrementArray = ["8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 AM","1:00 PM",
  "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM"] 
-
 for (let i = 0; i < timeIncrementArray.length; i++) {
-  $("#timeBlocks").append(generateTimeblock([i], "")
+  $("#timeBlocks").append(generateTimeblock(timeIncrementArray[i])
   )}
 
 //creating current date + Time
