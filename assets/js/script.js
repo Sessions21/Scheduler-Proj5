a700606769eeba8b5801af28e15aa07802ec469b
Scott Nichols <s21nichols@hotmail.com>
2021-11-24 22:50:54 -0700

    added background color change based on hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,46 @@
-function generateTimeblock (hour, value) {
-  var timeBlockRow = $("<div>")
-  timeBlockRow.addClass("row justify-content-center mb-1")
+function generateTimeblock (hour) {
+  var timeBlockRow = $("<div>");
+  timeBlockRow.addClass("row justify-content-center mb-1");
 
-  var timeBlockHour = $("<div>")
-  timeBlockHour.addClass("col-2 mr-2 bg-warning p-2 text-center font-weight-bold")
-  timeBlockHour.text(hour)
+  var timeBlockHour = $("<div>");
+  timeBlockHour.addClass("col-2 mr-2 bg-secondary p-2 text-center font-weight-bold");
+  timeBlockHour.text(hour);
 
-  var timeBlockDetail = $("<div>")
-  timeBlockDetail.addClass("col-6 bg-dark border-right border-left border-light text-light p-2")
+  var timeBlockDetail = $("<div>");
+  timeBlockDetail.addClass("col-6 border-right border-left border-light p-2");
+
+  var momentHour = moment(hour, 'hh:mm A').hour();
+  var currentHour = moment().hour();
+
+  if(momentHour < currentHour) {
+    timeBlockDetail.addClass("past");
+  }
+    else if (momentHour > currentHour) {
+    timeBlockDetail.addClass("future");
+  }
+  else if (momentHour === currentHour) {
+    timeBlockDetail.addClass("present");
+  }
   
-  var timeBlockInput = $("<textarea>")
-  timeBlockInput.attr("type", "text")
-  timeBlockInput.css('color', 'white')
-  timeBlockInput.css('width', "100%")
-  timeBlockInput.val(value)
-  timeBlockInput.attr("placeholder", "Enter task detail here")
-  timeBlockDetail.append(timeBlockInput)
+  var timeBlockInput = $("<textarea>");
+  timeBlockInput.attr("type", "text");
+  timeBlockInput.css('color', 'white');
+  timeBlockInput.css('width', "100%");
+  timeBlockInput.attr("placeholder", "Enter task detail here");
+  timeBlockDetail.append(timeBlockInput);
   if (localStorage.getItem(hour)) {
     timeBlockInput.val(localStorage.getItem(hour))
   }
 
 
-  var timeBlockSave = $("<div>")
-  timeBlockSave.addClass("col-2 ml-2 bg-info text-light text-center p-2")
-  var saveIcon = $("<i>")
-  saveIcon.addClass("fas fa-save fa-4x button")
-  timeBlockSave.append(saveIcon)
+  var timeBlockSave = $("<div>");
+  timeBlockSave.addClass("col-2 ml-2 bg-info text-light text-center p-2");
+  var saveIcon = $("<i>");
+  saveIcon.addClass("fas fa-save fa-4x button");
+  timeBlockSave.append(saveIcon);
   timeBlockSave.click(function () {
     localStorage.setItem(hour, timeBlockInput.val() )
-  })
+  });
 
 
   timeBlockRow.append(timeBlockHour, timeBlockDetail, timeBlockSave)
@@ -36,11 +48,17 @@ function generateTimeblock (hour, value) {
 }
 
 //Hour Increments for tasks
-var timeIncrementArray = ["8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 AM","1:00 PM",
- "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM"] 
+var timeIncrementArray = ["08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM","01:00 PM",
+ "02:00 PM", "03:00 PM", "04:00 PM", "05:00 PM", "06:00 PM"] 
 for (var i = 0; i < timeIncrementArray.length; i++) {
   $("#timeDisplay").append(generateTimeblock(timeIncrementArray[i])
   )};
+
+
+
+  // for (var i = 0; i < 9; i++) {
+  //   $("#timeDisplay").append(generateTimeblock(index+10,)) 
+  //   }
 
 
 //Current time display at page top on load
